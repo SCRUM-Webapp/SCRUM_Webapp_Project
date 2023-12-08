3b48ae41202de2c1e199865fc301519e579849b0
Vasco Pogade <118736868+VascoPogade@users.noreply.github.com>
2023-12-08 17:46:41 +0100

    Created navigation bar

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
 from flask import Flask, render_template, redirect, url_for
+from flask_bootstrap import Bootstrap5
 
 app = Flask(__name__)
 
@@ -6,9 +7,11 @@ app.config.from_mapping(
     BOOTSTRAP_BOOTSWATCH_THEME = 'quartz'
 )
 
+bootstrap = Bootstrap5(app)
+
 @app.route('/')
 def index():
-    return redirect(url_for('login'))
+    return render_template('base.html')
 
 @app.route('/Login')
 def login():
@@ -17,6 +20,10 @@ def login():
 @app.route('/Product_Backlog')
 def product_backlog():
     return render_template('Product_Backlog.html')
+
+@app.route('/Product_Backlog/<int:ticket_id>')
+def ticket(ticket_id):
+    return render_template('Ticket.html', ticket_id=ticket_id)
 
 @app.route('/Sprint_Planning')
 def sprint_planning():
