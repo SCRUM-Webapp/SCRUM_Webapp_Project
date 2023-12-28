8e9c6f65b818df24df069dfe9fb46c4efe839835
Jobstix <njobstvogt@googlemail.com>
2023-12-28 16:01:06 +0100

    ticket_id automatic increment

--- db.py
+++ db.py
@@ -10,12 +10,12 @@ db.init_app(app)
 
 class Ticket(db.Model):
     __tablename__ = 'ticket'
-    ticket_id = db.Column(db.Integer, primary_key=True, index=True)
+    ticket_id = db.Column(db.Integer, primary_key=True, autoincrement=True, index=True)
     ticket_name = db.Column(db.String(80), nullable=False)
     sprint_number = db.Column(db.Integer) 
     workload = db.Column(db.Float)
     description = db.Column(db.Text)
-    ticket_status = db.Column(db.String(12)) #done, in progress etc.
+    ticket_status = db.Column(db.String(14), default='Sprint Backlog') #done, in progress etc.
 
 with app.app_context():
     db.create_all()
@@ -36,7 +36,7 @@ def insert_sample():
     
 
     # Create sample ticket items
-    ticket1 = Ticket(ticket_id=1,ticket_name='Build logic to attach database to UI',sprint_number=1, workload=10, description='Needs to be implemented', ticket_status='in Progress')
+    ticket1 = Ticket(ticket_name='Build logic to attach database to UI',sprint_number=1, workload=10, description='Needs to be implemented', ticket_status='in Progress')
 
     # Add all objects to the queue and commit them to the database
     db.session.add(ticket1)
