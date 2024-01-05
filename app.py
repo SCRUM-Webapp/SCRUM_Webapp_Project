10aa33a775e11fcd7c7f70b1ff6eafac3e871d5a
Vasco Pogade <118736868+VascoPogade@users.noreply.github.com>
2024-01-05 14:37:25 +0100

    added abort

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
 from datetime import datetime
-from flask import Flask, render_template, redirect, url_for, request, flash
+from flask import Flask, render_template, redirect, url_for, request, flash, abort
 from flask_bootstrap import Bootstrap5
 from flask_login import LoginManager, login_user, login_required, logout_user, current_user
 from werkzeug.security import generate_password_hash, check_password_hash
