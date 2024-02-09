2a83b28bbf65f8add3124c5687bb0bd121ea13b5
Jobstix <njobstvogt@googlemail.com>
2024-02-09 16:07:08 +0100

    added comments, deleted profile.html, fixed small errors

--- db.py
+++ db.py
@@ -4,11 +4,14 @@ from sqlalchemy import orm
 from app import app
 from flask_login import UserMixin
 
+# configure the database URI
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///ticket_database.db' 
 
+# initialize SQLAlchemy with the flask app
 db = SQLAlchemy()
 db.init_app(app) 
 
+# ticket table
 class Ticket(db.Model):
     __tablename__ = 'ticket'
     ticket_id = db.Column(db.Integer, primary_key=True, autoincrement=True, index=True)
@@ -20,6 +23,7 @@ class Ticket(db.Model):
     assigned = db.Column(db.String(30))
     notes = db.Column(db.Text)
 
+# user table
 class User(UserMixin, db.Model):
     __tablename__ = 'user'
     id = db.Column(db.Integer, primary_key=True, autoincrement=True)
@@ -27,9 +31,11 @@ class User(UserMixin, db.Model):
     email = db.Column(db.String(80), nullable=False)
     password = db.Column(db.String(128), nullable=False)
 
+# create all tables
 with app.app_context():
     db.create_all()
 
+# CLI command to initalize the database
 @click.command('init-db')
 def init():  
     with app.app_context():
@@ -37,7 +43,8 @@ def init():
         db.create_all()
     click.echo('Database has been initialized.')
 
-app.cli.add_command(init)  #able to run flask init-db 
+#able to run flask init-db 
+app.cli.add_command(init)  
 
 def insert_sample():
     pass
